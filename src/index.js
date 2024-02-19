6365bd2e2c1b32deb08ec1e4790c0824ad7285b2
Ed <silva.email@yahoo.com>
2024-02-19 07:50:24 -0500

    Add styling for images and create reusable Card component

--- src/index.js
+++ src/index.js
@@ -1,33 +1,30 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const Card = (props) =>{
+return <div>
+<h2>{props.name}</h2>
+<img
+  src={props.img}
+  alt="avatar_img"
+/>
+<p>{props.tel}</p>
+<p>{props.email}</p> </div>
+}
+
 ReactDOM.render(
   <div>
     <h1>My Contacts</h1>
+       <Card name = "Beyonce" 
+       img = "https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg" 
+       tel = "+123 456 789" 
+       email = "b@byonce.com" />
 
-    <h2>Beyonce</h2>
-    <img
-      src="https://blackhistorywall.files.wordpress.com/2010/02/picture-device-independent-bitmap-119.jpg"
-      alt="avatar_img"
-    />
-    <p>+123 456 789</p>
-    <p>b@beyonce.com</p>
+       <Card name ="Sweet Daddy" 
+       img = "https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/a8b6c784-2e7f-42db-9f80-639737e36155/d9pel32-d3acd41d-12ce-4169-ba6f-1e9aeaa634ad.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcL2E4YjZjNzg0LTJlN2YtNDJkYi05ZjgwLTYzOTczN2UzNjE1NVwvZDlwZWwzMi1kM2FjZDQxZC0xMmNlLTQxNjktYmE2Zi0xZTlhZWFhNjM0YWQucG5nIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.QbgwpVprInztGn9DIldi5taGIdTWjGQdFd78ROlnCNY" 
+       tel = "+57 123 456 5567" 
+       email = "sweet@daddy.com" />
 
-    <h2>Jack Bauer</h2>
-    <img
-      src="https://pbs.twimg.com/profile_images/625247595825246208/X3XLea04_400x400.jpg"
-      alt="avatar_img"
-    />
-    <p>+987 654 321</p>
-    <p>jack@nowhere.com</p>
-
-    <h2>Chuck Norris</h2>
-    <img
-      src="https://i.pinimg.com/originals/e3/94/47/e39447de921955826b1e498ccf9a39af.png"
-      alt="avatar_img"
-    />
-    <p>+918 372 574</p>
-    <p>gmail@chucknorris.com</p>
   </div>,
   document.getElementById("root")
 );
